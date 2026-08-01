9ffcdb425cc2d7c711425adb4ebb36a0a75df086
gisella <dimitui.gisella@student.auf.edu.ph>
2026-08-01 18:43:14 +0800

    Update thank-you.php to include email and hashed password

--- lab2a/thank-you.php
+++ lab2a/thank-you.php
@@ -4,12 +4,12 @@ require "helpers/helper-functions.php";
 
 session_start();
 
-$contact_number = $_POST['contact_number'];
-$program = $_POST['program'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 $agree = $_POST['agree'];
 
-$_SESSION['contact_number'] = $contact_number;
-$_SESSION['program'] = $program;
+$_SESSION['email'] = $email;
+$_SESSION['password'] = $password;
 $_SESSION['agree'] = $agree;
 
 $form_data = $_SESSION;
