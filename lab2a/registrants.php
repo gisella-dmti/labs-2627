168d4245073bc52f01c60997e371b4afd3a3b711
gisella <dimitui.gisella@student.auf.edu.ph>
2026-08-02 00:04:19 +0800

    Display registrants from CSV in a table
    Added PHP code to read from 'registrations.csv' and display data in an HTML table.

--- lab2a/registrants.php
+++ lab2a/registrants.php
@@ -1,1 +1,31 @@
+<?php
+$file = fopen("registrations.csv", "r");
+?>
 
+<table border="1">
+<tr>
+    <th>Name</th>
+    <th>Birthday</th>
+    <th>Age</th>
+    <th>Contact</th>
+    <th>Sex</th>
+    <th>Program</th>
+    <th>Address</th>
+    <th>Email</th>
+</tr>
+
+<?php
+while (($row = fgetcsv($file)) !== false) {
+    echo "<tr>";
+
+    foreach ($row as $cell) {
+        echo "<td>$cell</td>";
+    }
+
+    echo "</tr>";
+}
+
+fclose($file);
+?>
+
+</table>
