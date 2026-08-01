0a56ad3bf797298f238ebd5d59e26a174bd8e673
gisella <dimitui.gisella@student.auf.edu.ph>
2026-08-01 19:20:42 +0800

    Change birthdate input type to 'date'
    Updated the birthdate input field to use type 'date' for better user experience.

--- lab2a/step-1.php
+++ lab2a/step-1.php
@@ -32,7 +32,7 @@ session_start();
           <input type="text" name="fullname" placeholder="John Doe">
           
           <label>Birthdate</label>
-          <input type="text" value="<?= $formattedBirthday ?>" readonly>
+          <input type="date" name="birthdate">
 
           <label>Contact Number</label>
           <input type="text" name="contact_number" placeholder="+639123456789" />
