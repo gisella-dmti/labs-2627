f553e2c5a2fdfdeb020caf68fbe9becc54926404
gisella <dimitui.gisella@student.auf.edu.ph>
2026-08-16 19:58:56 +0800

    Add file upload inputs for PDF and audio
    Added input fields for uploading PDF and audio files.

--- lab3b/index.php
+++ lab3b/index.php
@@ -34,6 +34,12 @@
     <p class="p-card__content">
         <input type="file" name="pdf_file" accept=".pdf" />
     </p>
+</div> 
+            <div class="p-card">
+    <h3>Audio File</h3>
+    <p class="p-card__content">
+        <input type="file" name="audio_file" accept=".mp3" />
+    </p>
 </div>
         <div>
             <button>
