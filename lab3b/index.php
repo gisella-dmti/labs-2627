d83f59d3a00fc9042b4be82dcade26dbdf7a1b9a
gisella <dimitui.gisella@student.auf.edu.ph>
2026-08-16 19:19:42 +0800

    Add form action and method for file upload

--- lab3b/index.php
+++ lab3b/index.php
@@ -21,7 +21,7 @@
   <div class="col">
     <h4>File Upload</h4>
 
-    <form>
+    <form action="uploaded.php" method="POST" enctype="multipart/form-data">
         <div class="p-card">
             <h3>Text File</h3>
             <p class="p-card__content">
