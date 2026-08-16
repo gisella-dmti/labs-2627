7d579238ea60169b66e688caced4b1933bb33379
gisella <dimitui.gisella@student.auf.edu.ph>
2026-08-16 15:06:07 +0800

    Fix PHP syntax issues in helpers.php

--- lab3a/helpers.php
+++ lab3a/helpers.php
@@ -34,7 +34,7 @@ function compute_score($answers = []) {
 
     $score = 0;
     for ($i = 0; $i < MAX_QUESTION_NUMBER; $i++) {
-     if ($correct_answers[$i] == ($answers[$i] ?? '')) {
+    if (($correct_answers[$i] ?? '') == ($answers[$i] ?? '')) {
     $score += 100;
         }
     }
